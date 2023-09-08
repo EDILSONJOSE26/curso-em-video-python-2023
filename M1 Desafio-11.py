69b3372d813e870a4907dc04263ea3cea4768162
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2023-09-08 19:17:44 -0300

    Update em M1 Desafio-11.py

--- M1 Desafio-11.py
+++ M1 Desafio-11.py
@@ -7,5 +7,5 @@ area = altura * largura
 tinta = area / 2
 
 print(f'A area da parede é de {area:.2f}m²')
-print(f'A quantidade de tinta recomendada é de {tinta:.2f}l')
+print(f'A quantidade de tinta recomendada é de {tinta:.2f} l')
 
