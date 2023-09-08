469d6dcc3f5b1ada4ddaec1bc120b259c3978c73
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2023-09-08 18:48:20 -0300

    fazendo upload

---
+++ M1 Desafio-11.py
@@ -0,0 +1,11 @@
+
+
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a da altura da parede: '))
+
+area = altura * largura
+tinta = area / 2
+
+print(f'A area da parede é de {area}m²')
+print(f'A quantidade de tinta recomendada é de {tinta:.2f}l')
+
